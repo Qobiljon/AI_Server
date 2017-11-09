c5c5e94353c6e7d4b221676d592e2a47616b04b0
Qobiljon <qobiljon.toshnazarov@gmail.com>
2017-11-09 14:35:13 +0900

    Final Changes.

--- main.py
+++ main.py
@@ -1,53 +1,129 @@
-import numpy
+import os
 import pickle
-from os.path import isfile
-from pybrain3.tools.shortcuts import buildNetwork
-from pybrain3.supervised.trainers import BackpropTrainer
+import numpy
 from pybrain3.datasets import SupervisedDataSet, UnsupervisedDataSet
 from pybrain3.structure import LinearLayer
+from pybrain3.supervised.trainers import BackpropTrainer
+from pybrain3.tools.shortcuts import buildNetwork
 
-# region Constants
-HISTORY_FILENAME = 'history.txt'
-NETDUMP_FILENAME = 'networkdump.txt'
 
-OBSERVE_LENGTH = 10
-PREDICT_LENGTH = 1
-# endregion
+class Tools:
+	@staticmethod
+	def objdump(dmp_obj, dmp_filename):
+		dump = open(dmp_filename, 'wb')
+		pickle.dump(dmp_obj, dump)
+		dump.close()
 
-# region Populate input file
-# fp = open('history.txt', 'w')
-# for x in range(100):
-#     fp.write("%d " % (22 + random.randrange(-1, 2, 1)))
-# fp.close()
-# endregion
+	@staticmethod
+	def objrecv(dmp_filename):
+		dump = open(dmp_filename, 'rb')
+		dmp_obj = pickle.load(dump)
+		dump.close()
+		return dmp_obj
 
-# region Initialize
-net = None
-if isfile(NETDUMP_FILENAME):
-    netDumpFile = open(NETDUMP_FILENAME, 'rb')
-    net = pickle.load(netDumpFile)
-    netDumpFile.close()
-else:
-    inp = numpy.loadtxt(HISTORY_FILENAME, int)
-    inputLength = len(inp)
-    trainDataSet = SupervisedDataSet(OBSERVE_LENGTH, PREDICT_LENGTH)
-    for n in range(inputLength):
-        if n + (OBSERVE_LENGTH - 1) + PREDICT_LENGTH < inputLength:
-            trainDataSet.addSample(inp[n:n + OBSERVE_LENGTH], inp[n + 1:n + 1 + PREDICT_LENGTH])
-    net = buildNetwork(OBSERVE_LENGTH, 20, PREDICT_LENGTH, outclass=LinearLayer, bias=True, recurrent=True)
-    trainer = BackpropTrainer(net, trainDataSet)
-    trainer.trainEpochs(100)
-# endregion
 
-# region Test
-ts = UnsupervisedDataSet(OBSERVE_LENGTH, )
-ts.addSample([21, 22, 23, 22, 21, 21, 22, 23, 23, 24])
+class CategoryAdvisor:
+	# region Notes on using this class
+	# In order to use dataset, history must contain >2*OBSERVE_LENGTH (which is >20)
+	# endregion
 
-print(int(net.activateOnDataset(ts)[0][0]))
-# endregion
+	# region Constants
+	PROJECT_DIR = 'myweek-ai-data'
 
-# region Save the network
-netDumpFile = open(NETDUMP_FILENAME, 'wb')
-pickle.dump(net, netDumpFile)
-netDumpFile.close()
-# endregion
+	NET_EXT = 'netdmp'
+	DST_EXT = 'dstdmp'
+
+	OBSERVE_LENGTH = 5
+	PREDICT_LENGTH = 1
+	# endregion
+
+	# region Variables
+	bprnetw = None
+	dataset = None
+	user = None
+	category_id = None
+
+	# endregion
+
+	@staticmethod
+	def create(user, category_id, network=None, dataset=None):
+		res = CategoryAdvisor()
+
+		res.user = user
+		res.category_id = category_id
+
+		if network is None:
+			res.bprnetw = buildNetwork(CategoryAdvisor.OBSERVE_LENGTH, 20, CategoryAdvisor.PREDICT_LENGTH, outclass=LinearLayer, bias=True, recurrent=True)
+		else:
+			res.bprnetw = network
+
+		if dataset is None:
+			res.dataset = SupervisedDataSet(CategoryAdvisor.OBSERVE_LENGTH, CategoryAdvisor.PREDICT_LENGTH)
+		else:
+			res.dataset = dataset
+
+		return res
+
+	@staticmethod
+	def recover(user, category_id):
+		if not CategoryAdvisor.is_backed_up(user, category_id):
+			return False
+
+		res = CategoryAdvisor()
+
+		res.user = user
+		res.category_id = category_id
+		res.bprnetw = Tools.objrecv('~/%s/%s.%s' % (res.PROJECT_DIR, user.username, res.NET_EXT))
+		res.dataset = Tools.objrecv('~/%s/%s.%s' % (res.PROJECT_DIR, user.username, res.DST_EXT))
+
+		return res
+
+	@staticmethod
+	def is_backed_up(user, category_id):
+		return \
+			os.path.exists('~/%s' % CategoryAdvisor.PROJECT_DIR) and \
+			os.path.isfile('~/%s/%s-%d.%s' % (CategoryAdvisor.PROJECT_DIR, user.username, category_id, CategoryAdvisor.NET_EXT)) and \
+			os.path.isfile('~/%s/%s-%d.%s' % (CategoryAdvisor.PROJECT_DIR, user.username, category_id, CategoryAdvisor.DST_EXT))
+
+	def backup(self):
+		# create backup files in home directory
+		if not os.path.exists('~/%s' % self.PROJECT_DIR):
+			os.makedirs('~/%s' % self.PROJECT_DIR)
+
+		Tools.objdump(dmp_obj=self.bprnetw, dmp_filename='~/%s/%s-%d.%s' % (self.PROJECT_DIR, self.user, self.category_id, self.NET_EXT))
+		Tools.objdump(dmp_obj=self.dataset, dmp_filename='~/%s/%s-%d.%s' % (self.PROJECT_DIR, self.user, self.category_id, self.DST_EXT))
+
+	def retrain_complete(self, history_complete):
+		if len(history_complete) >= CategoryAdvisor.OBSERVE_LENGTH:
+			self.dataset.clear()
+
+			data_length = len(history_complete)
+
+			for n in range(data_length):
+				if n + (CategoryAdvisor.OBSERVE_LENGTH - 1) + CategoryAdvisor.PREDICT_LENGTH < data_length:
+					self.dataset.addSample(history_complete[n:n + CategoryAdvisor.OBSERVE_LENGTH], history_complete[n + 1:n + 1 + CategoryAdvisor.PREDICT_LENGTH])
+
+			trainer = BackpropTrainer(self.bprnetw, self.dataset)
+			trainer.trainEpochs(100)
+
+			return True
+		else:
+			return False
+
+	def retrain_single(self, value):
+		inp = numpy.append(self.dataset['input'][-1][1:], self.dataset['target'][-1])
+		self.dataset.addSample(inp, [value])
+
+		trainer = BackpropTrainer(self.bprnetw, self.dataset)
+		trainer.trainEpochs(100)
+
+	def calculate(self):
+		ts = UnsupervisedDataSet(CategoryAdvisor.OBSERVE_LENGTH, )
+		ts.addSample(self.dataset['input'][-1])
+		return int(self.bprnetw.activateOnDataset(ts)[0][0])
+
+
+if __name__ == '__main__':
+	advisor = CategoryAdvisor.create(None, None)
+	advisor.retrain_complete([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+	print(advisor.calculate())
